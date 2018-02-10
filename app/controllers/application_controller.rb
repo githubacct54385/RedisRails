5515008f805f540d97883a560f609a82ae624b7c
Alex <alexbarke002@gmail.com>
2018-02-10 23:35:51 +0530

    rubocop code edits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def throttled_api_message(client_ip)
-    msg = "Whoa there!  "
+  def throttled_api_message
+    msg = 'Whoa there!  '
     msg += "You've pinged the server way too many times.  "
-    msg += "The cooldown period is #{blocking_timespan} seconds"
+    msg + "The cooldown period is #{blocking_timespan} seconds"
   end
 
   def remote_ip
@@ -37,55 +37,49 @@ class ApplicationController < ActionController::Base
   end
 
   def rails_warning(client_ip)
-    msg = "Overheat: User with id #{user_key_func(client_ip)} is over usage limit."
+    msg = 'Overheat: User with id '
+    msg += "#{user_key_func(client_ip)} is over usage limit."
     Rails.logger.warn msg
   end
 
-  def ShouldBlockUser(client_ip)
-    blocked_user = blocked_user_key_func(client_ip)
-    if RedisModule.redis.get(blocked_user)
-      return true
-    else
-      return false
-    end
-  end
-
-  def ExceededRequestsCount(num_requests, allowed_requests)
+  def exceeded_requests_count(num_requests, allowed_requests)
     num_requests > allowed_requests
   end
 
-  def UserIsNotBlocked(blocked_user_key)
+  def user_is_not_blocked(blocked_user_key)
     RedisModule.redis.get(blocked_user_key) != 1
   end
 
   # Returns true
   def track_api_usage(client_ip)
     # time-span to count the requests (in seconds)
-    watching_timespan=60
+    watching_timespan = 60
     # maximum request allowed within the time-span
-    allowed_requests=30
+    allowed_requests = 30
 
-    #compose key for counting requests
-    user_key="counting_#{client_ip}"
+    # compose key for counting requests
+    user_key = "counting_#{client_ip}"
     # compose key for identifying blocked users
-    blocked_user_key="locked_#{client_ip}"
+    blocked_user_key = "locked_#{client_ip}"
 
     # check if the user already has a counter
     if RedisModule.redis.get(user_key)
       # main action: increment counter
-      num_requests=RedisModule.redis.incr(user_key)
+      num_requests = RedisModule.redis.incr(user_key)
       print_details(client_ip, num_requests)
 
       # check limit
-      if ExceededRequestsCount(num_requests, allowed_requests) && UserIsNotBlocked(blocked_user_key)
+      if exceeded_requests_count(num_requests, allowed_requests) &&
+         user_is_not_blocked(blocked_user_key)
         # write something into the log file for alerting
-        Rails.logger.warn "Overheat: User with id #{client_ip} is over usage limit."
+        rails_warning(client_ip)
         # mark the user as "blocked"
-        RedisModule.redis.set(blocked_user_key,1) if RedisModule.redis.get(blocked_user_key) != 1
+        if RedisModule.redis.get(blocked_user_key) != 1
+          RedisModule.redis.set(blocked_user_key, 1)
+        end
         # make the blocking expiring itself after the defined cool-down period
-        RedisModule.redis.expire(blocked_user_key,blocking_timespan)
+        RedisModule.redis.expire(blocked_user_key, blocking_timespan)
       end
-
     else
       # no key for counting exists yet - so set a new one with ttl
       RedisModule.redis.set(user_key, 1)
--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,19 +1,24 @@
 # class for testing redis
 class MyController < ApplicationController
   def index
+    # get the IP Address of the user who called this method
     client_ip = remote_ip
-
-    puts client_ip
-    # Check for API Throttling
+    # check for API Throttling
     track_api_usage(client_ip)
+    # get the key of the user who would be blocked
+    blocked_user_key = "locked_#{client_ip}"
+    handle_response(blocked_user_key, client_ip)
+  end
+end
 
-    blocked_user_key="locked_#{client_ip}"
-    if RedisModule.redis.get(blocked_user_key)
-      render status: 429,
-             json: { message: throttled_api_message(client_ip) }
-    else
-      msg = "Client #{client_ip} /// Server Condition: Not Throtted."
-      render html: msg
-    end
+# handles server response with redis throttling
+def handle_response(blocked_user_key, client_ip)
+  # if user IP is blocked, show a message
+  if RedisModule.redis.get(blocked_user_key)
+    render status: 429,
+           json: { message: throttled_api_message }
+  else # everything is fine
+    msg = "Client #{client_ip} /// Server Condition: Not Throtted."
+    render html: msg
   end
 end
